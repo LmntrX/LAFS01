ef219e0fe1a77434dd2d6ba09cee586304361afe
Nihal Ismail <nihalismailk@gmail.com>
2016-06-29 19:34:16 +0530

    Added Tic tac toe and Video view

---
+++ SQLite DB/app/src/main/java/me/nihalismail/sqlitedb/Main2Activity.java
@@ -0,0 +1,29 @@
+
+package me.nihalismail.sqlitedb;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase("UsersDB",MODE_PRIVATE,null);
+        String select = "Select * from USERS";
+        Cursor cursor = sqLiteDatabase.rawQuery(select,null);
+        if (cursor.getCount()>0){
+            cursor.moveToFirst();
+            do {
+                textView.setText(textView.getText() + "\n" + cursor.getString(1));
+            }while (cursor.moveToNext());
+        }
+
+    }
+}
---
+++ VideoView/app/src/main/java/com/lmntrx/android/nihal/videotest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lmntrx.android.nihal.videotest;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
